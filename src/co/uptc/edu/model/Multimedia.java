f65a1fb6796613a4c6c35cd311dc46a2733edca5
Antr4ss <harold.guerrero02@uptc.edu.co>
2023-11-19 15:07:29 -0500

    Implemente la opcion de que el admin pueda crear peliculas

---
+++ src/co/uptc/edu/model/Multimedia.java
@@ -0,0 +1,89 @@
+package co.uptc.edu.model;
+
+import java.util.Timer;
+
+public class Multimedia {
+
+    private String tittle;
+    private String category;
+    private String details;
+    private int releaseYear;
+    private int duration;
+
+    Timer timer = new Timer();
+
+    public Multimedia() {
+    }
+
+    public Multimedia(String tittle, String category, String details, int releaseYear, int duration) {
+        this.tittle = tittle;
+        this.category = category;
+        this.details = details;
+        this.releaseYear = releaseYear;
+        this.duration = duration;
+    }
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void play(int duration) {
+        try {
+
+            System.out.println("playing");// agregar segundos
+            Thread.sleep(duration);
+
+            // Así, se da la impresión de que se ejecuta cada cierto tiempo
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stopPlay() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Multimedia: Titulo=" + tittle + ", categoria=" + category + ", detalles=" + details + ", año de lanzamiento="
+                + releaseYear + ", duracion=" + duration +"mins.";
+    }
+
+}
