bd0d0fb54215559a3bcc2114aea6deed68d6e4c0
felipe12l <andres.luna01@uptc.edu.co>
2023-11-19 20:51:52 -0500

    modifed comented utilities

--- src/co/uptc/edu/utilities/UserUtilities.java
+++ src/co/uptc/edu/utilities/UserUtilities.java
@@ -2,7 +2,10 @@ package co.uptc.edu.utilities;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+/*** 
+ *@author FelipeL
+    *  this class is used to validate actions and attributes of the user class in the controllers.
+ ***/
 public class UserUtilities {
     public boolean validPassword(String password){
         String regex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$";
