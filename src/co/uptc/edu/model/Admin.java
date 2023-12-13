55d2d5a715743f8f713b3ee6010c5b39b951e8ba
Valentina Vega <valentina.vega01@uptc.edu.co>
2023-12-12 22:07:45 -0500

    admin and chapter documentation

--- src/co/uptc/edu/model/Admin.java
+++ src/co/uptc/edu/model/Admin.java
@@ -1,37 +1,78 @@
 package co.uptc.edu.model;
 
+/**
+ * The Admin class represents an administrator in the platform.
+ */
 public class Admin {
-    private String username, password= "", email;
+    private String username, password = "", email;
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
+    /**
+     * Constructor for the Admin class.
+     *
+     * @param username Username of the administrator.
+     * @param password Password of the administrator.
+     * @param email    Email address of the administrator.
+     */
     public Admin(String username, String password, String email) {
         this.username = username;
         this.password = password;
         this.email = email;
     }
 
+    /**
+     * Gets the email address of the administrator.
+     *
+     * @return Email address of the administrator.
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Sets the email address of the administrator.
+     *
+     * @param email Email address of the administrator.
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * Gets the username of the administrator.
+     *
+     * @return Username of the administrator.
+     */
+
     public String getUsername() {
         return username;
     }
+
+    /**
+     * Sets the username of the administrator.
+     *
+     * @param username Username of the administrator.
+     */
 
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Gets the password of the administrator.
+     *
+     * @return Password of the administrator.
+     */
     public String getPassword() {
-        return password; 
+        return password;
     }
 
+    /**
+     * Sets the password of the administrator.
+     *
+     * @param password Password of the administrator.
+     */
     public void setPassword(String password) {
         this.password = password;
     }
-       
 
 }
--- src/co/uptc/edu/model/Chapter.java
+++ src/co/uptc/edu/model/Chapter.java
@@ -1,35 +1,65 @@
 package co.uptc.edu.model;
 
+/**
+ * The Chapter class represents an individual chapter of a series.
+ * It includes information such as the name and duration of the chapter.
+ */
 public class Chapter {
     private String name;
     private int duration;
 
-
+    /**
+     * Constructor for the Chapter class.
+     *
+     * @param name     Name of the chapter.
+     * @param duration Duration of the chapter.
+     */
     public Chapter(String name, int duration) {
         this.name = name;
         this.duration = duration;
     }
 
-    
+    /**
+     * Gets the name of the chapter.
+     *
+     * @return Name of the chapter.
+     */
     public String getName() {
         return name;
     }
 
-
+    /**
+     * Sets the name of the chapter.
+     *
+     * @param name Name of the chapter.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
-
+    /**
+     * Gets the duration of the chapter.
+     *
+     * @return Duration of the chapter.
+     */
     public int getDuration() {
         return duration;
     }
 
-
+    /**
+     * Sets the duration of the chapter.
+     *
+     * @param duration Duration of the chapter.
+     */
     public void setDuration(int duration) {
         this.duration = duration;
-    } 
-    
+    }
+
+    /**
+     * Returns a string representation of the Chapter object.
+     *
+     * @return A string representation of the Chapter object.
+     */
     @Override
     public String toString() {
         return "Chapter{" +
