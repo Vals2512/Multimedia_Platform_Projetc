7148829a55095102a20ec640a22ba2b56ed0b858
MauricioBarretoArenas <148931812+MauricioBarretoArenas@users.noreply.github.com>
2023-12-13 09:32:33 -0500

    modifique las categorias en el modelo -controlador-vista

---
+++ src/co/uptc/edu/model/database/CategoryDatabase.java
@@ -0,0 +1,41 @@
+package co.uptc.edu.model.database;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import co.uptc.edu.model.Category;
+
+public class CategoryDatabase {
+
+    public static final CategoryDatabase CATEGORY_DATABASE = new CategoryDatabase();
+
+    private List<Category> categories = new ArrayList<>();
+
+    public CategoryDatabase() {
+        // Predefinir las categorias
+    }
+
+    // las categorias deben ser unicas, una categoria es igual a otra si tiene el
+    // mismo nombre, solo agrega una categoria si no existe.
+    public boolean add(Category category) {
+        if (categories.contains(category)) {
+            return false;
+        }
+
+        categories.add(category);
+        return true;
+    }
+
+    public boolean remove(Category category) {
+        return categories.remove(category);
+    }
+
+    public Category search(String name) {
+        for (Category category : categories) {
+            if (category.getName().equalsIgnoreCase(name)) {
+                return category;
+            }
+        }
+        return null;
+    }
+}
