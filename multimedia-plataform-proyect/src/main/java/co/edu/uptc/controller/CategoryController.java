61b6575ad5871e382b5853d23dc9068108b047cd
felipe12l <andres.luna01@uptc.edu.co>
2024-02-13 17:04:40 -0500

    added read method

--- multimedia-plataform-proyect/src/main/java/co/edu/uptc/controller/CategoryController.java
+++ multimedia-plataform-proyect/src/main/java/co/edu/uptc/controller/CategoryController.java
@@ -1,8 +1,22 @@
 package co.edu.uptc.controller;
 
 import co.edu.uptc.model.Category;
+import co.edu.uptc.util.FileManager;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class CategoryController {
+    ArrayList<Category> categories;
+    private FileManager fm;
+    private final static String FILE="categories";
+    public CategoryController() {
+        categories=new ArrayList<>();
+        fm=new FileManager();
+        fm.createFile(FILE);
+    }
+
     public void updateName(Category category, String name) {
         category.setName(name);
     }
@@ -15,8 +29,10 @@ public class CategoryController {
     }
 
     public Category create(String name, String description) {
-        Category category = new Category(name, description); 
-
+        Category category = new Category(name, description);
+        categories.add(category);
+        Type type =new TypeToken<ArrayList<Category>>(){}.getType();
+        fm.addObject(FILE,categories,type);
         return category; 
     }
 }
