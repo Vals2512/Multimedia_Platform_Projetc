657ca50d04eeed7b6fc4d5508a8b5a973cad0fa2
Valentina Vega <valentina.vega01@uptc.edu.co>
2024-03-11 20:52:18 -0500

    Le puse estilo a la vista de main

--- multimedia-plataform-proyect/src/main/java/co/edu/uptc/view/SingInView.java
+++ multimedia-plataform-proyect/src/main/java/co/edu/uptc/view/SingInView.java
@@ -1,6 +1,5 @@
 package co.edu.uptc.view;
 
-
 import co.edu.uptc.controller.FileManagement;
 import co.edu.uptc.model.Plan;
 import javafx.beans.binding.Bindings;
@@ -39,8 +38,9 @@ public class SingInView implements Initializable {
     @FXML
     Button backButton;
     PaymentView p;
+
     @FXML
-    public void seeFirstPassword(){
+    public void seeFirstPassword() {
         if (password1.isSelected()) {
             showPassword1.setVisible(true);
             showPassword1.textProperty().bind(Bindings.concat(passwordF1.getText()));
@@ -50,8 +50,9 @@ public class SingInView implements Initializable {
             password1.setText("Show");
         }
     }
+
     @FXML
-    public void seeSecondPassword(){
+    public void seeSecondPassword() {
         if (password2.isSelected()) {
             showPassword2.setVisible(true);
             showPassword2.textProperty().bind(Bindings.concat(passwordF2.getText()));
@@ -61,26 +62,29 @@ public class SingInView implements Initializable {
             password2.setText("Show");
         }
     }
+
     @FXML
     void passwordFieldKeyTyped2(KeyEvent event) {
         showPassword2.textProperty().bind(Bindings.concat(passwordF2.getText()));
     }
+
     @FXML
     void passwordFieldKeyTyped1(KeyEvent event) {
         showPassword1.textProperty().bind(Bindings.concat(passwordF1.getText()));
     }
 
     public void send() throws IOException {
-        if (passwordF1.getText().equals(passwordF2.getText())){
-        if (passwordF1.getText().matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$")){
-           changePaymentView();
+        if (passwordF1.getText().equals(passwordF2.getText())) {
+            if (passwordF1.getText().matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$")) {
+                changePaymentView();
 
-        }else {
-            errorLabel.setStyle("-fx-text-fill: Red");
-            errorLabel.setText("su contraseña no cumple con tener uno de estos requisitos: \n mas de 8 caracteres\n tener una mayuscula \n una minuscula \n un caracter númerico ");
-            errorLabel.setWrapText(true);
-        }
-        }else {
+            } else {
+                errorLabel.setStyle("-fx-text-fill: Red");
+                errorLabel.setText(
+                        "Su contraseña no cumple con tener uno de estos requisitos: \n mas de 8 caracteres\n tener una mayuscula \n una minuscula \n un caracter númerico ");
+                errorLabel.setWrapText(true);
+            }
+        } else {
             errorLabel.setStyle("-fx-text-fill: Red");
             errorLabel.setText("Las contraseñas no coinciden");
             errorLabel.setWrapText(true);
@@ -94,7 +98,7 @@ public class SingInView implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        p=new PaymentView();
+        p = new PaymentView();
         Main.setResizable(true);
         showPassword1.setVisible(false);
         showPassword2.setVisible(false);
